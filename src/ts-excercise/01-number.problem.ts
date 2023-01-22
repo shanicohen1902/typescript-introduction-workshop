b65912256840374362162ad2c146d490b1b198f4
Shani Cohen <shani.cohen@tikalk.com>
2023-01-22 15:43:59 +0200

    remove tmps

--- src/ts-excercise/01-number.problem.ts
+++ src/ts-excercise/01-number.problem.ts
@@ -8,12 +8,3 @@ it("Should add the two numbers together", () => {
   expect(addTwoNumbers(2, 4)).toEqual(6);
   expect(addTwoNumbers(10, 10)).toEqual(20);
 });
-
-
-function myFunction(myStringArg, myNumberArg: any)
-{
-	// code
-  return "ggg"
-}
-
-const t:string = myFunction("ggg",7);
